ecf3eb8375a46446e68750eb11d2fbf9c6564f5e
wesleyafonsogomes <wesleyafonsogomes@yahoo.com.br>
2025-10-22 20:47:58 -0300

    numbers

---
+++ projeto/2_operacao_aritmetica/script.js
@@ -0,0 +1,12 @@
+console.log (typeof (2+3));
+console.log (typeof (5-12));
+console.log (typeof (4*4));
+console.log (typeof (5/3));
+console.log (typeof (20%2));
+console.log (typeof (21%2));
+
+console.log (20-10)
+console.log (10+0)
+console.log (100/10)
+console.log (2*5)
+console.log (10%4)

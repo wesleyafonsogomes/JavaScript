10f68869c66ff3bfe98cd0aa9e114c7abecff6ab
wesleyafonsogomes <wesleyafonsogomes@yahoo.com.br>
2025-11-29 17:56:52 -0300

    att

--- 13_ES6/index.js
+++ 13_ES6/index.js
@@ -5,84 +5,29 @@ const novo2 = 0;
 
 // arrow function
 
+const num = [1,2,3,4,5];
+
+const maioNum = (num) => Math.max(...num);
+// console.log(maioNum(num));
+
+const menorNum = (num) => Math.min(...num);
+// console.log(menorNum(num));
+
 const soma = (a,b) => a + b;
+// const saudacao = (nome) => "Olá, " + nome + "!";
+// console.log(saudacao("Wesley")); 
 
-const divisao = (a,b) => a / b;
+// exercicios filter 
 
-const saudacao = (nome) => {
-    console.log("Olá, " + nome);
-}
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const dobrar = (a,b) => (a + b)*2;
+const pares = numeros.filter(numero => numero % 2 === 0);
 
-const falar = () => {
-    return "Oi, eu sou uma arrow function"
-}
+const impares = numeros.filter((numero) => numero % 2 !== 0);
 
-// retornar array de objetos
+const maioresQue5 = numeros.filter((numero) => numero > 5);
 
-const carros = () => [{id: 1, marca: "Ferrari"}, {id:2, marca: "Jeep"}, {id:3, marca: "Maveric"}];
+const menorQue5 = numeros.filter((numero) => numero < 5);
+console.log(menorQue5);
 
-// Treino 
-
-const arrow1 = () => console.log("teste arrow");
-
-const arrow2 = () => {return "teste arrow2"}
-
-const arrow3 = (a,b) => (a + b) / 3
-
-// Filter 
-
-const arr = [1,2,3,4,5,6,7,8,9];
-
-const hightNumbers = arr.filter((num) => {
-    if(num >= 5) {
-        return num;
-    }
-});
-
-
-const lowNumbers = arr.filter((num) => {
-    if(num < 5) {
-        return num;
-    }
-});
-
-const pares = arr.filter((num) => {
-    if(num % 2 == 0) {
-        return num;
-    }
-});
-
-
-const impares = arr.filter((num) => {
-    if(num % 2 !== 0) {
-        return num;
-    }
-});
-
-// array de obj
-
-const alunos = [
-    {nome: "Wesley", disponivel: true},
-    {nome: "Maria", disponivel: false},
-    {nome: "João", disponivel: false},
-    {nome: "Nayara", disponivel: true},
-    {nome: "Nathalia", disponivel: false}
-];
-
-const alunosDisponiveis = alunos.filter((aluno) => aluno.disponivel);
-
-// console.log(alunosDisponiveis);
-
-const alunosIndisponiveis = alunos.filter((aluno) => !aluno.disponivel);
-
-// console.log(alunosIndisponiveis);
-
-const alunoWesley = alunos.filter((aluno) => aluno.nome === "Wesley");
-
-console.log(alunoWesley);
-
-
-
-
+const numero5 = numeros.filter((numero) => numero === 5);
